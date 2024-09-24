e114c04389425fdd8ea3d3491f7542e5b5ee10f4
B Tej <tez0231@gmail.com>
2024-09-24 20:45:46 +0530

    Update index.js

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,27 +1,20 @@
-/*var express = require('express');
-var router = express.Router();
-
- GET home page. 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
-*/
-/*const express = require('express');
-const router = express.Router();
-const ctrlMain = require('../controllers/main');
-/* GET homepage. 
-router.get('/', ctrlMain.index);
-module.exports = router; */
 const express = require('express');
 const router = express.Router();
 const ctrlLocations = require('../controllers/locations');
 const ctrlOthers = require('../controllers/others');
+const ctrlResults = require('../controllers/results');
+const ctrlAuth = require('../controllers/login'); // Assuming you have an auth controller
+
 /* Locations pages */
 router.get('/', ctrlLocations.homelist);
 router.get('/location', ctrlLocations.locationInfo);
 router.get('/location/review/new', ctrlLocations.addReview);
+
 /* Other pages */
 router.get('/about', ctrlOthers.about);
+router.get('/results', ctrlResults.results);
+
+/* Login page */
+router.get('/login', ctrlAuth.login); // Route to the login page
+
 module.exports = router;
